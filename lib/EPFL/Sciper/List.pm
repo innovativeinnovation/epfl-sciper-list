aaff949e997e61ac7c27b9f00a05d98cb273f5de
William Belle <william.belle@gmail.com>
2017-08-11 08:25:42 +0200

    init module

---
+++ lib/EPFL/Sciper/List.pm
@@ -0,0 +1,117 @@
+package EPFL::Sciper::List;
+
+use 5.006;
+use strict;
+use warnings;
+
+=head1 NAME
+
+EPFL::Sciper::List - The great new EPFL::Sciper::List!
+
+=head1 VERSION
+
+Version 0.01
+
+=cut
+
+our $VERSION = '0.01';
+
+
+=head1 SYNOPSIS
+
+Quick summary of what the module does.
+
+Perhaps a little code snippet.
+
+    use EPFL::Sciper::List;
+
+    my $foo = EPFL::Sciper::List->new();
+    ...
+
+=head1 EXPORT
+
+A list of functions that can be exported.  You can delete this section
+if you don't export anything, such as for a purely object-oriented module.
+
+=head1 SUBROUTINES/METHODS
+
+=head2 function1
+
+=cut
+
+sub function1 {
+}
+
+=head2 function2
+
+=cut
+
+sub function2 {
+}
+
+=head1 AUTHOR
+
+William Belle, C<< <william.belle at gmail.com> >>
+
+=head1 BUGS
+
+Please report any bugs or feature requests to C<bug-epfl-sciper-list at rt.cpan.org>, or through
+the web interface at L<http://rt.cpan.org/NoAuth/ReportBug.html?Queue=EPFL-Sciper-List>.  I will be notified, and then you'll
+automatically be notified of progress on your bug as I make changes.
+
+
+
+
+=head1 SUPPORT
+
+You can find documentation for this module with the perldoc command.
+
+    perldoc EPFL::Sciper::List
+
+
+You can also look for information at:
+
+=over 4
+
+=item * RT: CPAN's request tracker (report bugs here)
+
+L<http://rt.cpan.org/NoAuth/Bugs.html?Dist=EPFL-Sciper-List>
+
+=item * AnnoCPAN: Annotated CPAN documentation
+
+L<http://annocpan.org/dist/EPFL-Sciper-List>
+
+=item * CPAN Ratings
+
+L<http://cpanratings.perl.org/d/EPFL-Sciper-List>
+
+=item * Search CPAN
+
+L<http://search.cpan.org/dist/EPFL-Sciper-List/>
+
+=back
+
+
+=head1 ACKNOWLEDGEMENTS
+
+
+=head1 LICENSE AND COPYRIGHT
+
+Copyright 2017 William Belle.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    L<http://www.apache.org/licenses/LICENSE-2.0>
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+
+
+=cut
+
+1; # End of EPFL::Sciper::List
